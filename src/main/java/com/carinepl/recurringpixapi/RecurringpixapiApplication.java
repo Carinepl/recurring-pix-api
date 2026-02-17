b7a611921f7a195d41cfe4731072733b26c1c78a
Carinepl <Carinepl@users.noreply.github.com>
2026-02-17 19:33:02 -0300

    chore: setup Spring Boot 3.2.3 with PostgreSQL and fix dependencies

---
+++ src/main/java/com/carinepl/recurringpixapi/RecurringpixapiApplication.java
@@ -0,0 +1,13 @@
+package com.carinepl.recurringpixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecurringpixapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecurringpixapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carinepl/recurringpixapi/RecurringpixapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carinepl.recurringpixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecurringpixapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
